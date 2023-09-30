ea8fcdf990c481869435cb19baa1889ab7e2b92e
Tom <tomliem93@gmail.com>
2023-09-30 10:01:35 -0400

    feat:

--- src/app/components/Whatsapp/Whatsapp.tsx
+++ src/app/components/Whatsapp/Whatsapp.tsx
@@ -7,7 +7,7 @@ const Whatsapp = () => {
   return (
     <div className="fixed bottom-4 right-4 z-30">
       <a href={waUrl} className="items-center flex flex-row cursor-pointer">
-        <div className="text-white text-lg py-1 px-2 bg-[#25D366] border-white border-y-4 border-l-4">Cek Harga Perdana</div>
+        <div className="text-white text-lg py-1 px-2 bg-[#25D366] border-white border-y-4 border-l-4">Remax Solitaire Ratinah</div>
         <div className="bg-white rounded-lg">
           <FaWhatsappSquare size={80} color="#25D366" />
         </div>
